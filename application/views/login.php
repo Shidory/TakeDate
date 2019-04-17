06ff373d6105d6e5cb9027a09739625748e1abbf
MarioMajila <mariustshipichik@gmail.com>
2019-04-17 14:55:36 +0100

    ADD form code

--- application/views/login.php
+++ application/views/login.php
@@ -5,6 +5,27 @@
         <title>LogIn</title>
     </head>
     <body>
-        
+        <div style="width: 50%; padding: 10px" class="container">
+            <?= form_open("logInAdmin/login_validation"); ?>
+                <p class="h3 text-center mb-4">Sign in</p>
+                <div style="margin-bottom: 5%" class="md-form">
+                    <i class="fa fa-envelope prefix grey-text"></i>
+                    <label for="defaultForm-email">username</label>
+                    <input type="text" name="username" id="defaultForm-email" class="form-control">
+                    <span class="text-danger"><?php echo form_error('username'); ?></span>
+                </div>
+            
+                <div style="margin-bottom: 10px" class="md-form">
+                    <i class="fa fa-lock prefix grey-text"></i>
+                    <label for="defaultForm-pass">Your password</label>
+                    <input type="password" name="password" id="defaultForm-pass" class="form-control">
+                    <span class="text-danger"><?php echo form_error('password'); ?></span>
+                </div>
+            
+                <div class="text-center">
+                    <button type="submit" style="background-color: rgb(30,90,93); color: white; font-weight: bold" class="btn col-md-12">Login</button>
+                </div>
+            <?= form_close() ?>
+        </div>
     </body>
 </html>
