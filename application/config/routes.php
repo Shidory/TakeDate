126ad4cc4e43dc8ce6c36bf8242b0bf23cc02dfc
Simon MWEPU <simonmwepu@gmail.com>
2019-04-17 11:49:59 +0200

    [ADD]rooting

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+
+// Le rooting se fait ici
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
