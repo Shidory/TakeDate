0fae4d6f24e472f764f086d29af1bd1fbac70022
MarioMajila <mariustshipichik@gmail.com>
2019-04-17 15:13:30 +0100

    ADD login_validation function

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,8 +1,13 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
     class Login extends CI_Controller{
 
         public function index(){
+            $this->load->view('login');
+        }
+
+        public function login_validation(){
             
         }
     }
