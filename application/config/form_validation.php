14162432c26b99fe4086e74c06dc1a354ff71ad1
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-04-17 14:13:50 +0200

    [ADD] ajout des  rulers

---
+++ application/config/form_validation.php
@@ -0,0 +1,36 @@
+<?php
+
+    $config = array(
+        'clientController/clientView' => array(
+            array(
+                'field' => 'name',
+                'label' => 'Nom',
+                'rules' => 'required'
+            ),
+
+            array(
+                'field' => 'phone',
+                'label' => 'telephone',
+                'rules' => 'required'
+            ),
+
+            array(
+                'field' => 'mail',
+                'label' => 'email',
+                'rules' => 'required'
+            ),
+
+            array(
+                'field' => 'gender',
+                'label' => 'genre',
+                'rules' => 'required'
+            ),
+
+            array(
+                'field' => 'age',
+                'label' => 'age',
+                'rules' => 'required'
+            )
+        )
+    )
+?>
