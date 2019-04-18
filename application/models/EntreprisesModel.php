1d253a5687965a524bef8d5ae8b668f00a788539
Simon MWEPU <simonmwepu@gmail.com>
2019-04-18 14:08:19 +0200

    [ADD] ajout du constructeur

--- application/models/EntreprisesModel.php
+++ application/models/EntreprisesModel.php
@@ -2,9 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class EntreprisesModel extends CI_Model{
 
+    public function __construct(){
+
+        parent::__construct();
+        $table_rdv = "tb_rdv";
+    }
     public function reporter_rdv($data){
 
-        
+        $request = $this->db->insert('', $data);;
     }
 }
 ?>
