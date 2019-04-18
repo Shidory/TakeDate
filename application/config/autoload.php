8a22b51629c4d01c3ebd72580d6a3e541fe7c969
sarah diur <sarahddiur@gmail.com>
2019-04-18 11:17:14 +0200

    [ADD] ajout du parametre form dans les helpers

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'form_validation');
+$autoload['helper'] = array('url', 'file', 'form_validation', 'form');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('ClientModel', 'RdvModel', 'HoraireModel', 'AgentsModel', 'EntreprisesModel'; 'DepartementModel');
+$autoload['model'] = array('ClientModel', 'RdvModel', 'HoraireModel', 'AgentsModel', 'EntreprisesModel', 'DepartementModel');
