f1d2ed631e58cf0dcabbbb98b3a2274325fd8594
MarioMajila <mariustshipichik@gmail.com>
2019-04-18 12:46:26 +0100

    [UPDATE] requête qui recupère le username et le password dans la table tb_agent

--- application/models/entreprise/EntreprisesModel.php
+++ application/models/entreprise/EntreprisesModel.php
@@ -5,6 +5,8 @@
         $tb_agent = 'tb_agent';
 
         public function can_login($data){
-            
+            $this->db->where('username', $data['username']);
+            $this->db->where('pwd', $data['password']);
+            $query = $this->db->get($this->tb_agent);
         }
     }
