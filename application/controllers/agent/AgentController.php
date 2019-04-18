a5737ad54e0ec7bf473cc8f77be076ee626d81c5
carole Musonda <carolemwila@gmail.com>
2019-04-18 18:04:20 +0200

    [UPDATE] modification du if

--- application/controllers/agent/AgentController.php
+++ application/controllers/agent/AgentController.php
@@ -15,6 +15,29 @@ class AgentController extends CI_Controller {
 		$photo= $this->input->post('photo');
 		$username= $this->input->post('username');
 		$pwd= $this->input->post('pwd');
+		$confpwd=$this-><input->post('confpwd');
 		
+		if($this->form->run()){
+		$nomAgent= $this->input->post('nomAgent');
+		$telephone= $this->input->post('telephone');
+		$email= $this->input->post('email');
+		$photo= $this->input->post('photo');
+		$username= $this->input->post('username');
+		$pwd= $this->input->post('pwd');
+		$confpwd=$this-><input->post('confpwd');
+
+		if($pwd === $confpwd){
+			$data = array(
+				'nom'=> $nomAgent,
+				'telephone'=> $telephone,
+				'email'=> $email,
+				'photo'=> $photo,
+				'user'=> $username,
+				'pass'=>sha1($pwd)
+			);
+			$this->AgentModel->insert($data);
+			redirect(base_url)
+		}
+		}
 	}
 }
