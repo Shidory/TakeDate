70e2b4912597b28ca361beba981a0b8822586e8f
sarah diur <sarahddiur@gmail.com>
2019-04-18 14:21:15 +0200

    [ADD] creatin du formulaire d'annulation

---
+++ application/views/annuler_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="annuler_rdv" method="post">
+    <label for="motif">Motif</label>
+    <input type="text" name ="motif">
+    <label for="date">Date</label>
+    <input type="date" name ="date">
+    <label for="heure">Heure</label>
+    <input type="text" name ="heure">
+    <label for="duree">Durée</label>
+    <input type="text" name ="duree">
+    <input type="submit" value = "Annuler">
+    </form>
+</body>
+</html>
