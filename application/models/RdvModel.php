ef2f740a1de4b60f3713afb8a3f286e70790d627
Eden <raiveneden111@gmail.com>
2019-04-19 01:57:23 +0200

    ADD methode getIdAgent() dans la classe AgentsModel

--- application/models/RdvModel.php
+++ application/models/RdvModel.php
@@ -2,16 +2,23 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class RdvModel extends CI_Model{
 
-	//afficher les rendez vous en fonction des agents
-    public function afficherRDV($idAgent){
+	//afficher les rendez vous en fonction des agents [Caleb]
+    public function afficherRDV($idAgent)
+	{
     	$query=$this->db->select('NomClient, date, heure, dure')
 						->from('tb_rdv')
 						->join('tb_client, tb_rdv.idClient=tb_client.idClient')
 						->where('idAgent=:idAgent')
 						->get();
 	}
-	public  function annulerRDV(){
+	//cette methode parle d'elle même! j'espère que cela passera sans me prendre la tête [Caleb]
+	public  function annulerRDV($idRdv)
+	{
+		$data = array(
+			'etat'  => 'My date'
+		);
 
+		$this->db->replace('etat', 0);
 	}
 }
 ?>
--- application/views/RdvDejaPris.php
+++ application/views/RdvDejaPris.php
@@ -1,9 +1,17 @@
-<div style="background-color: #f4c9f0">
+<?php
+	require_once (anchor('RdvModel'));
+?>
+<div>
 	<h1>RDV accépté:</h1>
 	<div>
 		<span>Client: x</span>
 		<div>
-			<?= anchor('annulerRDV/annulerRdv/$id') ?>
+			<?php
+				$idAgent = $_SESSION['idAgent'];
+				$rdvModel=new RdvModel();
+				$id=$rdvModel->afficherRDV();
+				anchor('annulerRDV/annulerRdv/$id')
+			?>
 		</div>
 	</div>
 </div>
