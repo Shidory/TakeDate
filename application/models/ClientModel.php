db90cdbf31054bbde58db599125efc88e4c1b1dc
sarah diur <sarahddiur@gmail.com>
2019-04-19 13:57:12 +0200

    [ADD] ajout du commentaire

--- application/models/ClientModel.php
+++ application/models/ClientModel.php
@@ -3,9 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class ClientModel extends CI_Model{
 
     public function annuler_rdv($id,$data){
-
+      //requete d'annulation du rdv  
       $this->db->update('tb_rdv', $data);
       $this->db->where('id',$id);  
+    }
+    public function modifier_rdv$id,$data(){
+
     }
 }
 ?>
