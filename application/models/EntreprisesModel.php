5d97acbd8672e31154743568ffefdbba9365f5b4
kabengelescola21 <scola0021@gmail.com>
2019-04-19 09:59:10 +0200

    [ADD] add of function accepter_refuser_rdv in the model EntreprisesModel

--- application/models/EntreprisesModel.php
+++ application/models/EntreprisesModel.php
@@ -2,6 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class EntreprisesModel extends CI_Model{
 
-    
+    public __construct()
+    {
+        parent::__construct();
+    }
+    function accepter_refuser_rdv($id, $data)
+    {
+        $this->db->where('idRdv', $id);
+        $this->db->update('etat', $data);
+    }
 }
 ?>
