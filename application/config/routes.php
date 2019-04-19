39b50d7240a901d5047a56af7ad8181148e43adf
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-04-19 08:35:10 +0200

    [ADD] ajout du router horaire

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,3 +54,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['horaire'] = 'horaire/horaireEntreprise';
--- application/controllers/horaire/HoraireController.php
+++ application/controllers/horaire/HoraireController.php
@@ -2,6 +2,14 @@
 
     class HoraireController extends CI_Controller{
 
-        
+        public function horaireView(){
+            $this->load->view('horaire_view');
+        }
+
+        public function enregistrer(){
+
+            $jour = $this->input->post('jour');
+            $heure_debut = $this->input->post('heureDebut');
+        }
     }
 ?>
