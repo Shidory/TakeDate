d8bd5b9ae9d46cb5da605e783f5315d983c7f3ad
kabengelescola21 <scola0021@gmail.com>
2019-04-19 13:12:01 +0200

    [UPDATE]correction

--- application/controllers/entreprise/EntrepriseController.php
+++ application/controllers/entreprise/EntrepriseController.php
@@ -7,18 +7,46 @@ class EntrepriseController extends CI_Controller {
 	{
 		$this->load->view('accepter_view');
 	}
-	public function accepter_refuser($id)
+	public function accepter_controller(1)
 	{
-		$choix = $_POST['choix'];
-		$this->_rules();
-		 
-		$data = array(
-					'choix' => $_POST['choix']
-					);
-		for($i= 0, $i < $data.length(), $i++){
-			if($data[i] = 'accepter')
-			$this->EntreprisesModel->accepter_refuser_rdv($id, $data);
+		if(isset($motif, $date, $heure, $duree,$etat, $commentaire)){
+			$idClient = 2;
+			$idEntreprise = 2;
+			$motif = 'marché';
+			$date = '2019-04-25';
+			$heure = '07:30:00';
+			$duree = '3heures';
+			$etat = '1';
+			$commentaire = 'okey'; 
+			$data = array(
+				'idClient' => $idClient,
+				'idEntreprise' => $idEntreprise,
+				'motif' => $motif,
+				'date' => $date,
+				'heure' => $heure,
+				'duree' => $duree,
+				'etat' => "1",
+				'commentaire' => $commentaire
+			);
 		}
-		
+		$this->EntreprisesModel->accepter_refuser_rdv(1, $data);	
+	}
+	public function refuser_controller($id)
+	{
+		if(isset($id, $motif, $date, $heure, $duree, $etat, $commentaire)){
+			$data = array(
+				'idRdv' => $this->input->post('idRdv'),
+				'idClient' => $this->input->post('idClient'),
+				'idEntreprise' => $this->input->post('idEntreprise'),
+				'motif' => $this->input->post('motif'),
+				'date' => $this->input->post('date'),
+				'heure' => $this->input->post('heure'),
+				'duree' => $this->input->post('duree'),
+				'etat' => $this->input->post('etat'),
+				'commentaire' => $this->input->post('commentaire')
+			);
+		}
+		$this->EntreprisesModel->accepter_refuser_rdv($id, $data);	
 	}
 }
+?>
