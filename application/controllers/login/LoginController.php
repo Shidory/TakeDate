9a77e45455d9ced3ffa7ecf60d80dcdbacdaf93a
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-04-19 09:59:16 +0200

    [UPDATE] changement du chemin de la vue

--- application/controllers/login/LoginController.php
+++ application/controllers/login/LoginController.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
     class Login extends CI_Controller{
 
         public function index(){
-            $this->load->view('login');
+            $this->load->view('login/login');
         }
 
         public function login_validation(){
