616a6369b65c67c8347a21c776abcc8d56efce14
sarah diur <sarahddiur@gmail.com>
2019-04-19 11:45:34 +0200

    [ADD] creation des variables

--- application/controllers/client/ClientController.php
+++ application/controllers/client/ClientController.php
@@ -11,10 +11,27 @@ class ClientController extends CI_Controller {
 	public function annuler_rdv(){
 
 		$id = $this->input->get("id");
-		
+		$idClient = $this->input->get("idClient");
+		$idEntrprise = $this->input->get("idEntrprise");
+		$motif = $this->input->get("motif");
+		$date = $this->input->get("date");
+		$duree = $this->input->get("duree");
+		$etat = $this->input->get("etat");
+		$commentaire= $this->input->get("commentaire");
+		;
 		if (isset($id)){
 
+			$data = array (
+				'idClient',
+				'idEntreprise',
+				'motif'=>$motif,
+				'date',
+				'duree',
+				'etat' => '0',
+				'commentaire';
+			)
 			$this->ClientModel->annuler_rdv($id,$etat);
+
 		}
 	}
 }
