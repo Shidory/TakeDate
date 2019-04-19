bb3b03e0bc830fcb23f0f22b37685d9480a0172a
atthie <atthie27@gmail.com>
2019-04-19 09:34:20 +0200

    [NEW] creation de la methode get_Entreprise dans le fichier EntreprisesModel.php

--- application/models/EntreprisesModel.php
+++ application/models/EntreprisesModel.php
@@ -1,7 +1,13 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+
 class EntreprisesModel extends CI_Model{
 
+
+    public function get_Entreprise()
+    {
+        $this->db->select('*');
+        return $this->db->get('tb_entreprise')->result_array();
+    }
     
 }
 ?>
