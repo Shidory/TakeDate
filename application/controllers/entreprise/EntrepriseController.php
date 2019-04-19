3a13cd7fb7e3659fdd9fdcb1cc4fc334a7fcf942
Simon MWEPU <simonmwepu@gmail.com>
2019-04-19 11:44:23 +0200

    [ADD]etat à 1

--- application/controllers/entreprise/EntrepriseController.php
+++ application/controllers/entreprise/EntrepriseController.php
@@ -51,7 +51,42 @@
 	#######################################################
 	public function modifier_rdv(){
 
+		//Réccupération des données venant du formulaire
+		$idRdv = $this->input->get("idRdv");
 		
+		$motif = $this->input->post("motif");
+		$date = $this->input->post("date");
+		$heure = $this->input->post("heure");
+		$duree = $this->input->post("duree");
+		$commentaire = $this->input->post("commentaire");
+
+		//Vérification de l'existance des clés fournies
+		if(isset($motif, $date, $heure, $duree, $commentaire)){
+
+			$data = array(
+				
+				"idClient" => $idClient,
+				"idEntreprise" => $idEntreprise,
+				"motif" => $motif,
+				"date" => $date,
+				"heure" => $heure,
+				"duree" => $duree,
+				"etat" => "1",
+				"commentaire" => $commentaire
+			);
+
+			try{
+
+				//Appel de la méthdode reporter_rdv en lui passant  
+				//l'id et le tableau des valeurs en paramètre
+				$this->EntreprisesModel->reporter_rdv($idRdv, $data);
+			}
+			catch(Exception $e){
+
+				redirect('reporter_view');
+			}
+			
+		}
 	}
 
 	#######################################################
