e9978714a31dd7a60d01ccd299a340e7817d7d9b
carole Musonda <carolemwila@gmail.com>
2019-04-19 12:38:38 +0200

    [CREATE] creation de la methode de chaargement

---
+++ application/controllers/rdv/rdvController.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AgentController extends CI_Controller {
+
+	public function index()
+	{
+		//chargement de la page prendre rendez-vous dans la vue
+		$this->load->view('prendre_rdv');
+	}
+	public function prendre_rdv()
+	{
+		//recuperation des donnees dans la vue
+		$motif= $this->input->post('motif');
+		$date= $this->input->post('date');
+		$heure= $this->input->post('heure');
+		$duree= $this->input->post('duree');
+		$etat= $this->input->post('etat');
+		$commentaire= $this->input->post('commentaire');
+		$idEntreprise = 1;
+		$idClient = 1;
+
+		echo $commentaire, $date;
+		$data = array(
+						'motif'=> $motif,
+						'date'=> $date,
+						'heure'=> $heure,
+						'duree'=> $duree,
+						'etat'=> $etat,
+						'commentaire'=> $commentaire,
+						'idEntreprise' => $idEntreprise,
+						'idClient' => $idClient
+					);
+		$this->AgentsModel->prendre_rdv($data);		
+		
+	}
+}
