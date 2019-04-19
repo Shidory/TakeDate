adf20367534b8b7d6ec3f55eb35bb48b7f0954f5
carole Musonda <carolemwila@gmail.com>
2019-04-19 12:15:48 +0200

    [ADD] ajout d'un formulaire

--- application/views/prendre_rdv.php
+++ application/views/prendre_rdv.php
@@ -1,1 +1,22 @@
-xcv []
+<div>
+<h3 >Prendre un rendez-vous</h3>
+<?php echo form_open(''); ?>
+<div>
+    <p>Motif :</p>
+    <input type="text" name="motif"  class="champ" required>
+    <p>Date :</p>
+    <input type="date" name="date" required>
+    <p>Heure :</p>
+    <input type="text" name="heure" required>
+    <p>Durée :</p>
+    <input type="text" name="duree" required>
+    <p>Etat :</p>
+    <input type="text" name="etat" required>
+    <p>Commentaire :</p>
+    <input type="text" name="commentaire" required>
+    
+</div>
+<br>
+<input type="submit" value="Envoyer">
+<?php echo form_close() ?>
+</div>
