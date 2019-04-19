9a09f15df086f588612acebb472e4bbc14a1a4bd
kabengelescola21 <scola0021@gmail.com>
2019-04-19 13:10:57 +0200

    [UPDATE]update view

--- application/views/accepter_view.php
+++ application/views/accepter_view.php
@@ -6,69 +6,75 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>	 
+
+<body>
     <div>
         <div>
             <h3>rendez vous</h3>
         </div>
+        <form action="entreprise" method="post">
+
+                
         <table>
-  <thead>
-    <tr>
-      <th>idRdv</th>
-      <th>idClient</th>
-      <th>idEntreprise</th>
-      <th>motif</th>
-      <th>date</th>
-      <th>heure</th>
-      <th>duree</th>
-      <th>commentaire</th>
-      <th>choix</th>      
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>business</td>
-        <td>21/11/2001</td>
-        <td>11:22:21</td>
-        <td>23</td>
-        <td>va jouer au bille</td>
-        <td><input type="button" value="accepter">
-            <input type="button" value="refuser">
-        </td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>business</td>
-        <td>21/11/2001</td>
-        <td>11:22:21</td>
-        <td>23</td>
-        <td>va jouer au bille</td>
-        <td><input type="button" value="accepter">
-            <input type="button" value="refuser">
-        </td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>business</td>
-        <td>21/11/2001</td>
-        <td>11:22:21</td>
-        <td>23</td>
-        <td>va jouer au bille</td>
-        <td><input type="button" value="accepter">
-            <input type="button" value="refuser">
-        </td>
-    </tr>
-  </tbody>
-</table>
+        <thead>
+            <tr>
+            <th>idRdv</th>
+            <th>idClient</th>
+            <th>idEntreprise</th>
+            <th>motif</th>
+            <th>date</th>
+            <th>heure</th>
+            <th>duree</th>
+            <th>commentaire</th>
+            <th>choix</th>      
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>business</td>
+                <td>21/11/2001</td>
+                <td>11:22:21</td>
+                <td>23</td>
+                <td>va jouer au bille</td>
+                <td><input type="submit" value="accepter">
+                    <input type="submit" value="refuser">
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>business</td>
+                <td>21/11/2001</td>
+                <td>11:22:21</td>
+                <td>23</td>
+                <td>va jouer au bille</td>
+                <td><input type="submit" value="accepter">
+                    <input type="submit" value="refuser">
+                </td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>business</td>
+                <td>21/11/2001</td>
+                <td>11:22:21</td>
+                <td>23</td>
+                <td>va jouer au bille</td>
+                <td><input type="submit" value="accepter">
+                    <input type="submit" value="refuser">
+                </td>
+            </tr>
+        </tbody>
+        </table>
 
     </div>
+</form>	 
+    
    
 </body>
 </html>
