ee3538c867ebfb61fcdff61febd370a957dec1f4
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-04-21 22:04:54 +0200

    [ADD] ajout de la methode get_horaire

--- application/models/HoraireModel.php
+++ application/models/HoraireModel.php
@@ -2,12 +2,23 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
     class HoraireModel extends CI_Model{
 
+        public function __construct(){
+            parent::__construct();
+        }
+
         public function ajouter_horaire($data){
             $this->db->insert('tb_horaire',$data);
         }
         
-        public function get_horaire(){
-            
+        public function get_horaire($idAgent){
+            // recuperation de l'horaire par rapport a un agent
+            $req = $this->db->select('h.jour, h.heure_debut, h.heure_fin, a.nomAgent')
+                     ->from('tb_horaire as h')
+                     ->where('a.idAgent',$idAgent)
+                     ->join('tb_agent as a', 'a.idAgent = h.idAgent')
+                     ->get();  
+
+            return $req;
         }
     }
 ?>
