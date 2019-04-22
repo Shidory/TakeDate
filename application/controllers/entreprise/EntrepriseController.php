35ac83e1b960ac470335094b59d9657d6a23f866
kabengelescola21 <scola0021@gmail.com>
2019-04-22 12:28:16 +0200

    [ADD]ajout de methode accepter_refuser_rdv

--- application/controllers/entreprise/EntrepriseController.php
+++ application/controllers/entreprise/EntrepriseController.php
@@ -8,41 +8,49 @@ class EntrepriseController extends CI_Controller {
 		
 		$this->load->model("EntreprisesModel");
 		$data["fetch_data"] = $this->EntreprisesModel->fetch_data();
+		$data["rdv_data"] = $this->EntreprisesModel->accepter_refuser_rdv($id_rdv);
 		$this->load->view('accepter_view', $data);
 		$this->accepter_controller();
 	}
+
 	public function update_data()
 	{
 		$id_rdv = $this->uri->segment(3);
 		$this->load->model("EntreprisesModel");
-		$data["rdv_data"] = $this->EntreprisesModel->accepter_refuser_rdv($id_rdv);
+		$data['rdv_data'] = $this->EntreprisesModel->accepter_refuser_rdv($id_rdv) ;
 		$data["fetch_data"] = $this->EntreprisesModel->fetch_data();
-		$this->load->view('accepter_view', $data)
+		$this->load->view('accepter_view', $data);
 	}
-	public function form_validation(){
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules("motif", "motif",'required|alpha');
-		$this->form_validation->set_rules("date", "date",'required|alpha');
-		$this->form_validation->set_rules("heure", "heure",'required|alpha');
-		$this->form_validation->set_rules("duree", "duree",'required|alpha');
-		$this->form_validation->set_rules("etat", "etat",'required|alpha');
-		$this->form_validation->set_rules("commentaire", "commentaire",'required|alpha');
-		if($this->form_validation->run()){
-			$this->load->model("EntreprisesModel");
-			$date = array(
-				"motif"		=>$this->input->post("motif"),
-				"date"		=>$this->input->post("date"),
-				"heure"		=>$this->input->post("heure"),
-				"duree"		=>$this->input->post("duree"),
-				"etat"		=>$this->input->post("etat"),
-				"commentaire"=		=>$this->input->post("commentaire")
+
+	//La methode change l'etat d'un rendez-vous du cote Entreprise
+	public function accepter_refuser_rdv(){
+		$id = $this->input->get('id');
+		$state = $this->EntreprisesModel->get_state($id);
+		$stateValue = $this->input->get('state');
+
+		if($stateValue == 1){
+			$newState = "1";
+	
+			$data = array(
+				'idRdv' => $id,
+				'etat' => $newState
 			);
-			if($this->input->post("update")){
-				$this->EntreprisesModel->update_data($data, $this->input->post("hidden_id"));
-				redirect(base_url() . "EntrepriseController/updated");
-			}
-			$this->EntreprisesModel->
+	
+			$this->EntreprisesModel->update_data($id, $data);
+
 		}
+		else{
+			$newState = "0";
+	
+			$data = array(
+				'idRdv' => $id,
+				'etat' => $newState
+			);
+	
+			$this->EntreprisesModel->update_data($id, $data);
+
+		}
+		redirect(base_url());
 	}
 	
 	public function updated(){
