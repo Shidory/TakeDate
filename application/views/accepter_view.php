480acc696716b310bb85aa2a2bfea2fde6ce1f5e
kabengelescola21 <scola0021@gmail.com>
2019-04-22 10:02:06 +0200

    [ADD]form input

--- application/views/accepter_view.php
+++ application/views/accepter_view.php
@@ -7,70 +7,89 @@
     <title>Document</title>
 </head>
 
-<body>
-    <div>
+    <body>
         <div>
-            <h3>rendez vous</h3>
+            <div>
+                <h3>rendez vous</h3>
+            </div>         
+            <table>
+            <thead>
+                <tr>
+                <th>idRdv</th>
+                <th>idClient</th>
+                <th>idEntreprise</th>
+                <th>motif</th>
+                <th>date</th>
+                <th>heure</th>
+                <th>duree</th>
+                <th>etat</th>
+                <th>commentaire</th>
+                <th>choix1</th>  
+                <th>choix2</th>    
+                </tr>
+            </thead>
+            <tbody>
+            <?php 
+                if($fetch_data->num_rows() > 0){
+                    foreach($fetch_data->result() as $row)
+                    {    
+            ?>
+                <tr>
+                    <td><?php echo $row->idRdv; ?></td>
+                    <td><?php echo $row->idClient; ?></td>
+                    <td><?php echo $row->idEntreprise; ?></td>
+                    <td><?php echo $row->motif; ?></td>
+                    <td><?php echo $row->date; ?></td>
+                    <td><?php echo $row->heure; ?></td>
+                    <td><?php echo $row->duree; ?></td>
+                    <td><?php echo $row->etat; ?></td>
+                    <td><?php echo $row->commentaire; ?></td>
+                    <td>
+                       <a href="<?php echo base_url(); ?>EntrepriseController/update_data/<?php echo $row->idRdv; ?>">Accepter</a>
+                        
+                    </td>
+                    <td><a href="entreprise/refuser_controller/1">Refuser</a></td>
+                </tr>
+            <?php 
+                    }
+                }
+                else
+                {
+            ?>
+                <tr>
+                    <td>No data found</td>
+                </tr>
+                <?php 
+                }
+                ?>
+            </tbody>
+            </table>
+            <form action="EntrepriseController/form_validation" method="post">
+                <?php 
+                    if(isset($rdv_data)){
+                        foreach($rdv_data->result() as $row){
+                            ?>
+                            <label>idRdv</label>
+                            <input type="text" name="motif" value="<?php echo $row->motif?>"/><br>
+                            <label>date</label>
+                            <input type="date" name="date" value="<?php echo $row->date?>"/><br>
+                            <label>heure</label>
+                            <input type="datetime" name="heure" value="<?php echo $row->heure?>"/><br>
+                            <label>duree</label>
+                            <input type="text" name="duree" value="<?php echo $row->duree?>"/><br>
+                            <label>etat</label>
+                            <input type="text" name="etat" value="<?php echo $row->etat?>"/><br>
+                            <label>commentaire</label>
+                            <input type="text" name="commentaire" value="<?php echo $row->commentaire?>"/><br>
+                            <input type="hidden" name="hidden_id" value="<?php $row->id?>"/><br>
+                            <input type="submit" name="update" value="Update">
+                            <?php
+                        }
+                    }
+                ?>
+            </form>
+           
+
         </div>
-
-                
-        <table>
-        <thead>
-            <tr>
-            <th>idRdv</th>
-            <th>idClient</th>
-            <th>idEntreprise</th>
-            <th>motif</th>
-            <th>date</th>
-            <th>heure</th>
-            <th>duree</th>
-            <th>commentaire</th>
-            <th>choix</th>      
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>business</td>
-                <td>21/11/2001</td>
-                <td>11:22:21</td>
-                <td>23</td>
-                <td>va jouer au bille</td>
-                <td><input type="submit" value="accepter">
-                    <input type="submit" value="refuser">
-                </td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>business</td>
-                <td>21/11/2001</td>
-                <td>11:22:21</td>
-                <td>23</td>
-                <td>va jouer au bille</td>
-                <td><input type="submit" value="accepter">
-                    <input type="submit" value="refuser">
-                </td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>business</td>
-                <td>21/11/2001</td>
-                <td>11:22:21</td>
-                <td>23</td>
-                <td>va jouer au bille</td>
-                <td><input type="submit" value="accepter">
-                    <input type="submit" value="refuser">
-                </td>
-            </tr>
-        </tbody>
-        </table>
-
-    </div>
-</body>
+    </body>
 </html>
