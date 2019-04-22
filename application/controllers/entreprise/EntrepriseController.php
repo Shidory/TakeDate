110e92568806203c4343dcf2a3686b18a5ec767b
kabengelescola21 <scola0021@gmail.com>
2019-04-22 10:00:56 +0200

    [ADD]methode update_data

--- application/controllers/entreprise/EntrepriseController.php
+++ application/controllers/entreprise/EntrepriseController.php
@@ -5,48 +5,48 @@ class EntrepriseController extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('accepter_view');
+		
+		$this->load->model("EntreprisesModel");
+		$data["fetch_data"] = $this->EntreprisesModel->fetch_data();
+		$this->load->view('accepter_view', $data);
+		$this->accepter_controller();
 	}
-	public function accepter_controller(1)
+	public function update_data()
 	{
-		if(isset($motif, $date, $heure, $duree,$etat, $commentaire)){
-			$idClient = 2;
-			$idEntreprise = 2;
-			$motif = 'marché';
-			$date = '2019-04-25';
-			$heure = '07:30:00';
-			$duree = '3heures';
-			$etat = '1';
-			$commentaire = 'okey'; 
-			$data = array(
-				'idClient' => $idClient,
-				'idEntreprise' => $idEntreprise,
-				'motif' => $motif,
-				'date' => $date,
-				'heure' => $heure,
-				'duree' => $duree,
-				'etat' => "1",
-				'commentaire' => $commentaire
-			);
-		}
-		$this->EntreprisesModel->accepter_refuser_rdv(1, $data);	
+		$id_rdv = $this->uri->segment(3);
+		$this->load->model("EntreprisesModel");
+		$data["rdv_data"] = $this->EntreprisesModel->accepter_refuser_rdv($id_rdv);
+		$data["fetch_data"] = $this->EntreprisesModel->fetch_data();
+		$this->load->view('accepter_view', $data)
 	}
-	public function refuser_controller($id)
-	{
-		if(isset($id, $motif, $date, $heure, $duree, $etat, $commentaire)){
-			$data = array(
-				'idRdv' => $this->input->post('idRdv'),
-				'idClient' => $this->input->post('idClient'),
-				'idEntreprise' => $this->input->post('idEntreprise'),
-				'motif' => $this->input->post('motif'),
-				'date' => $this->input->post('date'),
-				'heure' => $this->input->post('heure'),
-				'duree' => $this->input->post('duree'),
-				'etat' => $this->input->post('etat'),
-				'commentaire' => $this->input->post('commentaire')
+	public function form_validation(){
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules("motif", "motif",'required|alpha');
+		$this->form_validation->set_rules("date", "date",'required|alpha');
+		$this->form_validation->set_rules("heure", "heure",'required|alpha');
+		$this->form_validation->set_rules("duree", "duree",'required|alpha');
+		$this->form_validation->set_rules("etat", "etat",'required|alpha');
+		$this->form_validation->set_rules("commentaire", "commentaire",'required|alpha');
+		if($this->form_validation->run()){
+			$this->load->model("EntreprisesModel");
+			$date = array(
+				"motif"		=>$this->input->post("motif"),
+				"date"		=>$this->input->post("date"),
+				"heure"		=>$this->input->post("heure"),
+				"duree"		=>$this->input->post("duree"),
+				"etat"		=>$this->input->post("etat"),
+				"commentaire"=		=>$this->input->post("commentaire")
 			);
+			if($this->input->post("update")){
+				$this->EntreprisesModel->update_data($data, $this->input->post("hidden_id"));
+				redirect(base_url() . "EntrepriseController/updated");
+			}
+			$this->EntreprisesModel->
 		}
-		$this->EntreprisesModel->accepter_refuser_rdv($id, $data);	
+	}
+	
+	public function updated(){
+		$this->index();
 	}
 }
 ?>
