74fd3b6b2841ba953380ca8b5ad2975944d1a49e
atthie <atthie27@gmail.com>
2019-04-26 15:10:06 +0200

    [ADD]Ajout du model get_Agent

--- application/models/AgentsModel.php
+++ application/models/AgentsModel.php
@@ -5,8 +5,15 @@
     public function ajouter_agent($data)
     {
         //insertion des donnees dans la base des donnees
-        
         $this->db->insert('tb_agent', $data);
+    }
+
+    public function get_Agent($idEntreprise)
+    {
+        $this->db->select('*');
+        $this->db->where('idEntreprise',$idEntreprise);
+        return $this->db->get('tb_agent')->result_array();
+       
     }
 }
 ?>
