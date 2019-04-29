f15be87dfceec64ea3abe3e4a74a534efbd93bb2
Simon MWEPU <simonmwepu@gmail.com>
2019-04-29 07:48:09 +0200

    [UPDATE]Réorganisation

--- application/models/EntreprisesModel.php
+++ application/models/EntreprisesModel.php
@@ -42,7 +42,7 @@ class EntreprisesModel extends CI_Model
         $this->db->update('tb_rdv', $data);
     }
     
-  ##########################################################################
+    #######################################################
     
     public function can_login($data)
     {
@@ -58,7 +58,7 @@ class EntreprisesModel extends CI_Model
         }
     }
   
-  ###########################################################################
+    #######################################################
     public function get_Entreprise()
     {
         //cette methode recupere tout les elements de la table tb_entreprise
@@ -66,8 +66,7 @@ class EntreprisesModel extends CI_Model
         return $this->db->get('tb_entreprise')->result_array();
     }
 
-    #########################################################################
-   
+    #######################################################
     public function get_Random_Entreprises()
     {
         //cette methode recupere une seule entreprise dans la table tb_entreprise d'une maniere aleatoire
@@ -76,5 +75,6 @@ class EntreprisesModel extends CI_Model
         $this->db->limit(1);
         return $this->db->get('tb_entreprise')->result_array();
     }
+
 }
 ?>
