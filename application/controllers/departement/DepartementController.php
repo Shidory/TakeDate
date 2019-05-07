971c793b1b1e2a257bae15b5dd5cc998f26148cb
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-05-07 16:45:41 +0200

    [ADD] creation de la methode index

--- application/controllers/departement/DepartementController.php
+++ application/controllers/departement/DepartementController.php
@@ -2,8 +2,21 @@
 
     class DepartementController{
 
+        public function index(){
+
+        }
+        
         public function enregstrer_departement(){
+            $id = (int) $this->session->userdata('id');
+            $nom = $this->input->post("name");
+
+            $data = array(
+                    'nomDepartement' => $nom,
+                    'idEntreprise' => $id
+                );
             
+            $this->DepartementModel->enregister($data);
+            redirect('');
         }
     }
 ?>
