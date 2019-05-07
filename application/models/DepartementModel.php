902283464f44a004b3799a69f11dc7a92071180a
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-05-07 16:55:27 +0200

    [ADD] creation de la methode enregistrer

--- application/models/DepartementModel.php
+++ application/models/DepartementModel.php
@@ -2,6 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class DepartementModel extends CI_Model{
 
-    
+    public function enregister($data){
+        $this->db->insert('tb_departement', $data);
+    }
 }
 ?>
