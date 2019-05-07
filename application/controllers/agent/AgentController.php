9e21ce9283d8ce8074585477fa3508a2a284a3aa
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-05-08 01:03:17 +0200

    [ADD] creation e la methode login

--- application/controllers/agent/AgentController.php
+++ application/controllers/agent/AgentController.php
@@ -7,40 +7,53 @@ class AgentController extends CI_Controller {
 	{
 		//chargement de la page ajouter_agent dans la vue
 		// var_dump($this->session->userdata);
-		var_dump($this->session->userdata('user'));
-		$users  = $this->session->userdata('user');
+		var_dump($this->session->userdata('id'));
+		var_dump($this->session->userdata('email'));
 
-		foreach($users as $user){
-			echo $user;
-		}
-
+		// foreach($users as $user){
+		// 	echo $user;
+		// }
 		$this->load->view('agent');
 	}
 	public function ajouter_agent()
 	{
 		//recuperation des donnees dans la vue
-		$nomAgent= $this->input->post('nomAgent');
-		$telephone= $this->input->post('telephone');
+		$nomAgent= $this->input->post('name');
+		$telephone= $this->input->post('phone');
 		$email= $this->input->post('email');
-		$photo= $this->input->post('photo');
+		// $photo= $this->input->post('photo');
 		$username= $this->input->post('username');
 		$pwd= $this->input->post('pwd');
+		$pwdconf = $this->input->post('confpwd');
 		$confpwd=$this->input->post('confpwd');
-		$idEntreprise = 1;
-		$idDept = 1;
+		$idEntreprise = $this->session->userdata('id');
+		$idDept = 3;
 
-		echo $nomAgent, $telephone;
-		$data = array(
-						'nomAgent'=> $nomAgent,
-						'telephone'=> $telephone,
-						'email'=> $email,
-						'photo'=> $photo,
-						'username'=> $username,
-						'pwd'=>sha1($pwd),
-						'idEntreprise' => $idEntreprise,
-						'idDept' => $idDept
-					);
-		$this->AgentsModel->ajouter_agent($data);		
+		if($pwd == $confpwd){
+			$pwd = sha1($pwd);
+			$data = array(
+				'nomAgent'=> $nomAgent,
+				'telephone'=> $telephone,
+				'email'=> $email,
+				// 'photo'=> $photo,
+				// 'username'=> $username,
+				'pwd'=>sha1($pwd),
+				'idEntreprise' => $idEntreprise,
+				'idDept' => $idDept
+			);
+			$this->AgentsModel->ajouter_agent($data);
+			redirect(base_url('/departement/DepartementController'));
+		}
+		else{
+			$data['error'] = "mot de passe incorrect";
+			$data['agents'] = $this->AgentsModel->get_Agent($idEntreprise);
+			$this->load->view('setting', $data);
+		}
+				
+		
+	}
+
+	public function login(){
 		
 	}
 	
