d83f6e13ba748edeb9d233cb9c18efa1f196da38
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-05-08 04:12:51 +0200

    [ADD] creation de la methode agent_login

--- application/controllers/login/LoginController.php
+++ application/controllers/login/LoginController.php
@@ -20,6 +20,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
             // Données venant de la base de données pour permettre la vérification de l'authentification
             $data_db  =   $this->EntreprisesModel->verification($data);
             $admin    =   $this->EntreprisesModel->get_entreprise_line($data);
+
+            
+            $agents_data   =   $this->AgentsModel->verification($data);
+            $agents         =   $this->AgentsModel->get_agents_line($data);
+
+            // var_dump($admin);
         
             if($data_db){
                 foreach($admin as $adm){
@@ -31,7 +37,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
                     $this->session->set_userdata('id', $data_session['id']);
                     $this->session->set_userdata('email', $data_session['email']);    
                 }
+                
                 redirect(base_url('/departement/DepartementController'));
+            }
+
+            else if($agents_data){
+                foreach($agents as $agent){
+
+                    $this->session->set_userdata('id', $agent->idAgent);
+                    $this->session->set_userdata('email', $agent->email);
+                }
+
+                redirect(base_url('/agent/AgentCOntroller'));
+
             }
             else{
                 $data['error'] = "Email ou mot de passe incorrect";
@@ -39,12 +57,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
             }
         }
 
-        // Fonction de déconnexion
+       
+        public function load_login_view(){
+            $data['error'] = "";
+            $this->load->view('agent_login',$data);
+        }
 
         // Fonction de déconnexion
 
         public function logout(){
-            $this->session->unset_userdata($session_data);//Destruction des valeurs de session
+            $this->session->unset_userdata('id');//Destruction des valeurs de session
+            $this->session->unset_userdata('email');
             redirect(base_url('login'));
         }
     }
